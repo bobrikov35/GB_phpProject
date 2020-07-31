e5ffdf3844910b0258f0a200e8dd215b8ba3ac7c
Aleksey A. Bobrikov <bobrikov.spb@ya.ru>
2020-07-31 20:27:49 +0300

    Update SERVICE::Paginator

--- src/services/Paginator.php
+++ src/services/Paginator.php
@@ -13,18 +13,19 @@ use app\repositories\Repository;
 class Paginator extends Service
 {
 
+    /**
+     * С В О Й С Т В А
+     */
+
     private array $items = [];
     private int $quantity = 0;
-    private int $itemsOnPage = 12;
+    private int $itemsOnPage = 9;
     private string $path = '/product/list/?page=';
 
+
     /**
-     * @param string $path
+     * S E T T E R ' Ы
      */
-    public function setPath(string $path): void
-    {
-        $this->path = $path;
-    }
 
     /**
      * @param Repository $repository
@@ -36,6 +37,48 @@ class Paginator extends Service
         $this->items = $repository->getItemsByPage($page, $this->itemsOnPage);
     }
 
+    /**
+     * @param string $path
+     */
+    public function setPath(string $path): void
+    {
+        $this->path = $path;
+    }
+
+
+    /**
+     * G E T T E R ' Ы
+     */
+
+    /**
+     * @return Entity[]|array
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+
+    /**
+     * П У Б Л И Ч Н Ы Е   Ф У Н К Ц И И
+     */
+
+    /**
+     * @return array
+     */
+    public function getUrls(): array
+    {
+        $urls = [];
+        for ($i = 1; $i <= $this->getPages(); $i++) {
+            $urls[$i] = $this->path . $i;
+        }
+        return $urls;
+    }
+
+
+    /**
+     * П Р И В А Т Н Ы Е   Ф У Н К Ц И И
+     */
 
     /**
      * @return int
@@ -47,26 +90,6 @@ class Paginator extends Service
             return (int)$result;
         }
         return 1;
-    }
-
-    /**
-     * @return array
-     */
-    public function getUrls(): array
-    {
-        $urls = [];
-        for ($i = 1; $i <= $this->getPages(); $i++) {
-            $urls[$i] = "{$this->path}{$i}";
-        }
-        return $urls;
-    }
-
-    /**
-     * @return Entity[]
-     */
-    public function getItems(): array
-    {
-        return $this->items;
     }
 
 }
