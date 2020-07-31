e68814f57853c08873bcea03d58d283b9ddf75b2
Aleksey A. Bobrikov <bobrikov.spb@ya.ru>
2020-07-31 22:25:43 +0300

    Update TRAIT::Singleton

--- src/traits/TSingleton.php
+++ src/traits/TSingleton.php
@@ -6,14 +6,31 @@ namespace app\traits;
 trait TSingleton
 {
 
+    /**
+     * С В О Й С Т В А
+     */
+
     private static $items;
 
 
-    protected function __construct() {}
-    protected function __clone() {}
-    protected function __wakeup() {}
+    /**
+     * М А Г И Ч Е С К И Е   Ф У Н К Ц И И
+     */
+
+    private function __construct() {}
+    private function __clone() {}
+    private function __wakeup() {}
 
 
+    /**
+     * П У Б Л И Ч Н Ы Е   Ф У Н К Ц И И
+     */
+
+    /**
+     * Возвращает экземпляр класса в единственном экземпляре
+     *
+     * @return mixed
+     */
     public static function getInstance()
     {
         if (empty(static::$items)) {
