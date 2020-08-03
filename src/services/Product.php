081a6f31062510d9bb1d5ad642135a1055a7c776
Aleksey A. Bobrikov <bobrikov.spb@ya.ru>
2020-08-03 12:38:50 +0300

    Update SERVICE::Product

--- src/services/Product.php
+++ src/services/Product.php
@@ -2,7 +2,7 @@
 
 namespace app\services;
 
-use app\entities\Product as EProduct;
+use app\entities\{Product as EProduct};
 
 
 /**
@@ -13,17 +13,91 @@ class Product extends Service
 {
 
     /**
-     * С И Н Т А К С И Ч Е С К И Й   С А Х А Р
+     * П У Б Л И Ч Н Ы Е   Ф У Н К Ц И И
      */
 
     /**
+     * Возвращает товар из базы данных по id
+     *
      * @param int $id
      * @return EProduct|null
      */
-    private function getSingleProduct(int $id)
+    public function getProduct(int $id)
     {
         return $this->container->repositoryProduct->getSingle($id);
     }
+
+    /**
+     * Возвращает товар с данными из POST-запроса
+     *
+     * @return EProduct
+     */
+    public function getProductFromPost()
+    {
+        $product = new EProduct();
+        $this->fillProductFromPost($product);
+        return $product;
+    }
+
+    /**
+     * Сохраняет товар в базе данных
+     *
+     * @param int $id
+     * @return bool|int
+     */
+    public function save(int $id)
+    {
+        if (empty($id)) {
+            $product = new EProduct();
+        } else {
+            $product = $this->getProduct($id);
+        }
+        $this->fillProductFromPost($product);
+        return $this->saveProduct($product);
+    }
+
+    /**
+     * Удаляет товар из базы данных
+     *
+     * @param int $id
+     * @return bool
+     */
+    public function delete(int $id): bool
+    {
+        if (empty($id)) {
+            return false;
+        }
+        if ($this->getProduct($id)) {
+            return $this->deleteProduct($id);
+        }
+        return false;
+    }
+
+
+    /**
+     * П Р И В А Т Н Ы Е   Ф У Н К Ц И И
+     */
+
+    /**
+     * Заполняет товар данными из post-запроса
+     *
+     * @param EProduct $product
+     */
+    private function fillProductFromPost(EProduct $product):void
+    {
+        $product->setName($this->getPost('name'));
+        $product->setTitle($this->getPost('title'));
+        $product->setDescription($this->getPost('description'));
+        $product->setImage($this->getPost('image'));
+        $product->setPrice($this->getPost('price'));
+        $product->setImagesFromString($this->getPost('images'));
+    }
+
+
+
+    /**
+     * С И Н Т А К С И Ч Е С К И Й   С А Х А Р
+     */
 
     /**
      * @param EProduct $product
@@ -41,68 +115,6 @@ class Product extends Service
     private function deleteProduct(int $id): bool
     {
         return $this->container->repositoryProduct->delete($id);
-    }
-
-    /**
-     * @param string $param
-     * @return array|mixed|null
-     */
-    private function getPost(string $param)
-    {
-        return $this->request->getPost($param);
-    }
-
-
-    /**
-     * П У Б Л И Ч Н Ы Е   Ф У Н К Ц И И
-     */
-
-    /**
-     * Заполняет товар данными из post-запроса
-     *
-     * @param EProduct $product
-     */
-    public function fillProductFromPost(EProduct $product):void
-    {
-        $product->setName($this->getPost('name'));
-        $product->setTitle($this->getPost('title'));
-        $product->setDescription($this->getPost('description'));
-        $product->setImage($this->getPost('image'));
-        $product->setPrice($this->getPost('price'));
-        $product->setImagesFromString($this->getPost('images'));
-    }
-
-    /**
-     * Сохранение товара
-     *
-     * @param int $id
-     * @return bool|int
-     */
-    public function save(int $id)
-    {
-        if (empty($id)) {
-            $product = new EProduct();
-        } else {
-            $product = $this->getSingleProduct($id);
-        }
-        $this->fillProductFromPost($product);
-        return $this->saveProduct($product);
-    }
-
-    /**
-     * @param int $id
-     * @return bool
-     */
-    public function delete(int $id): bool
-    {
-        if (empty($id)) {
-            return false;
-        }
-        $product = $this->getSingleProduct($id);
-        if (empty($product)) {
-            return false;
-        }
-        return $this->deleteProduct($id);
     }
 
 }
