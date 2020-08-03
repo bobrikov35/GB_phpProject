dd43bf4df6502b64aa8f291e28fe4061eb4a5f9f
Aleksey A. Bobrikov <bobrikov.spb@ya.ru>
2020-08-03 10:35:50 +0300

    Update CONTROLLER::Home

--- src/controllers/Home.php
+++ src/controllers/Home.php
@@ -10,11 +10,23 @@ namespace app\controllers;
 class Home extends Controller
 {
 
+    /**
+     * Д Е Й С Т В И Я
+     */
+
+    /**
+     * Действие по умолчанию
+     */
     protected function default_action()
     {
         $this->toLocation('/home/index');
     }
 
+    /**
+     * Выводит домашнюю страницу
+     *
+     * @return string
+     */
     protected function index_action()
     {
         return $this->render('home.twig', $this->getConfig());
